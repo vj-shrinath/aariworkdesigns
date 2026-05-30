88c5083795cf1b840c484b05ee25d7e66798bcc4
vjshrinath0 <itsyoboyvj@gmail.com>
2026-05-31 00:31:30 +0530

    Robust exclusion of main featured image from gallery

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -31,7 +31,8 @@ export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0] {
 
 
 export const GALLERY_QUERY = groq`*[_type == "post" && defined(slug.current)] {
-  "images": body[_type == "image" && !(alt match "workflow*")] {
+  "mainImageAsset": mainImage.asset._ref,
+  "images": body[_type == "image"] {
     "_id": _key,
     "title": ^.title,
     "slug": ^.slug,
@@ -39,9 +40,9 @@ export const GALLERY_QUERY = groq`*[_type == "post" && defined(slug.current)] {
       "asset": asset,
       "alt": alt
     },
-    "isFeatured": asset._ref == ^.mainImage.asset._ref || asset._ref == ^.mainImage.asset->_id
+    "assetRef": asset._ref
   }
-}.images[] [defined(mainImage) && isFeatured != true] {
+}.images[] [assetRef != ^.mainImageAsset] {
   "_id": _id,
   "title": title,
   "slug": slug,
